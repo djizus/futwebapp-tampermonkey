8bb54ca94e4de1168d3491c35c4a2cdd98f56f63
djizus <mehrcj@gmail.com>
2021-10-07 10:58:18 +0200

    v3.0

--- app/instant-bin-confirm/settings-entry.js
+++
@@ -1,8 +0,0 @@
-import { SettingsEntry } from '../core';
-
-export class InstantBinConfirmSettings extends SettingsEntry {
-  static id = 'instant-bin-confirm';
-  constructor() {
-    super('instant-bin-confirm', 'Instantly confirm Buy It Now dialog');
-  }
-}
--- fut/errors/index.js
+++
@@ -1,1 +0,0 @@
-export class ListPlayerError extends Error {}
